b2afb4dd645226c0220ddeef8dce78bd4fb4d3c3
charlotte0911 <charlotte.quirk.23@gmail.com>
2023-10-10 00:41:13 +0100

    final commit 9th Oct

--- pages/starwars.js
+++ pages/starwars.js
@@ -2,3 +2,49 @@ fetch('https://swapi.dev/api/people/')
 .then(resp => resp.json())
 .then(data => console.log(data))
 
+// // Define the async function
+// async function getapi(url) {
+   
+//     // Storing response
+//     const response = await fetch(url);
+   
+//     // Storing data in form of JSON
+//     var data = await response.json();
+//     console.log(data);
+    
+//     show(data);
+// }
+// // Calling that async function
+// getapi('https://swapi.dev/api/people/');
+ 
+
+// // Function to define innerHTML for HTML table
+// function show(data) {
+//     let tab = 
+//         `<tr>
+//           <th>Name</th>
+//           <th>Height</th>
+//           <th>Mass</th>
+//           <th>Hair Color</th>
+//           <th>Skin Color</th>
+//           <th>Eye Color</th>
+//           <th>Birth Year</th>
+//           <th>Gender</th>
+//          </tr>`;
+   
+//     // Loop to access all rows 
+//     for (let r of data.people) {
+//         tab += `<tr> 
+//     <td>${r.name} </td>
+//     <td>${r.height}</td>
+//     <td>${r.mass}</td> 
+//     <td>${r.hair_color}</td>  
+//     <td>${r.skin_color}</td>           
+//     <td>${r.eye_color}</td>    
+//     <td>${r.birth_year}</td>    
+//     <td>${r.gender}</td>    
+// </tr>`;
+//     }
+
+
+// }
